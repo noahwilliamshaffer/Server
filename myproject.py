ae5f693ce49f27846c7acf449a3d6209da2c35dc
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2022-10-08 19:55:53 -0400

    Added timedelta for temporary session data storage

--- myproject.py
+++ myproject.py
@@ -4,6 +4,7 @@
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
+from datetime import timedelta
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
