d71bc98d2058a8934cb40fbf79822ce9addbc166
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2022-10-08 16:33:17 -0400

    A small example of pulling information from URL

--- api.py
+++ api.py
@@ -1,2 +1,10 @@
 # 📁 API.py -----
 #implementing the API in flask
+
+from flask import flask, request
+
+//Requesting from a website
+@app.route(...) 
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
