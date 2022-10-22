40196266317e4ac667568539ed89558be59e3c0e
Noah Shaffer <noahwilliamshaffer@gmail.com>
2022-10-22 20:37:46 +0000

    Finished Models for our like and potentially comment

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from . import db
 from flask_login import userMixin
 from sqlaclhemy.sql import func
 
+#we need font awesome for incons for likes
 
 class User.(db.Model, UserMixin):
     id = db.column(db.integer, Primary_key = True)
@@ -13,9 +14,10 @@ class User.(db.Model, UserMixin):
 
      #connects post to user
      posts = db.relationship('Post',backref = 'User', passive deletes = True)
+     comments = db.relationship('Comment',backref = 'User', passive deletes = True)
+     likes = db.relationship('Like',backref = 'User', passive deletes = True)
 
-
-class Post (db.model):
+class Post (db.Model):
     id = db.column(db.integer, Primary_key = True)
     text = db.column(db.text, nullable = False)
     date_created = db.column(db.DateTime(timezone = True),defualt = func.now())
@@ -23,11 +25,20 @@ class Post (db.model):
 
     #connects comment to post
     comments = db.relationship('Comment',backref = 'Post', passive deletes = True)
+    likes = db.relationship('Like', backref = 'Post', passive deletes = True)
 
-
-class Comment (db.model):
+class Comment (db.Model):
     id = db.column(db.integer, Primary_key = True)
     text = db.column(db.string(200), nullable = False)
     date_created = db.column(db.DateTime(timezone = True),defualt = func.now())
     author = db.column(db.Integer, db.ForeignKey('user.id', ondelete = "CASCADE"), nullable = False)
     post_id =  db.column(db.Integer, db.ForeignKey('user.id', ondelete = "CASCADE"), nullable = False)
+
+class Like(db.Model):
+     id = db.column(db.integer, Primary_key = True)
+
+    #can't hurt to add even if we don't end up using it
+     date_created = db.column(db.DateTime(timezone = True),defualt = func.now())
+
+     author = db.column(db.Integer, db.ForeignKey('user.id', ondelete = "CASCADE"), nullable = False)
+     post_id =  db.column(db.Integer, db.ForeignKey('user.id', ondelete = "CASCADE"), nullable = False)
