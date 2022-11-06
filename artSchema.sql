e83f05470e653fe1e13852a4754098a440ddd650
Noah Shaffer <noahwilliamshaffer@gmail.com>
2022-11-06 21:37:07 +0000

    cant use text as a primary key for sql database, must use user ids

--- artSchema.sql
+++ artSchema.sql
@@ -1,10 +1,11 @@
 CREATE TABLE if not exists users
-   (email TEXT PRIMARY KEY,
+   (UserId INTEGER PRIMARY KEY,
+    email TEXT Not Null,
     name TEXT NOT NULL);
 
 CREATE TABLE if not exists likedArt
-   (	Users_Id TEXT NOT NULL,
+   (	ID TEXT NOT NULL,
 	title TEXT NOT NULL,
     	url TEXT NOT NULL,
-    	FOREIGN KEY (User_Id) REFERENCES users (email)
+    	FOREIGN KEY (ID) REFERENCES users(UserId)
     	ON DELETE CASCADE);
