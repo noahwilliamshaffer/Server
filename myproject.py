4946d250c15b6062f4829ed8dfc3faea0238107e
Marlee-Krause <mmk20a@my.fsu.edu>
2022-11-16 22:22:46 +0000

    THIS IS NOAH

--- myproject.py
+++ myproject.py
@@ -262,8 +262,12 @@ def UserProfiles():
     cursor = con.execute('SELECT id, title, url FROM items WHERE email = Email')
     items = cursor.fetchall()
     cursor.close()
-
-    return render_template("UserProfiles.html",email = Email,items = items, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    BIG = json.dumps(session.get("user"))
+    BIGGER = json.loads(BIG)
+    BIGGEST = BIGGER['userinfo']
+    EMAIL = BIGGEST['email']
+    if EMAIL in Admins:
+        return render_template("UserProfiles.html",email = Email,items = items, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 #the index function contains the way to call the html file that will be using the data being heald in our database ex
 #APP ROUTE FUNCTION FOR DATABASE CODE EXSAMPLE
 @app.route('/database')
