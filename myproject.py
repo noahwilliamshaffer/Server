8eaeb8cddd7c57930de458d7a9ed0de7e6ff2c1f
Marlee-Krause <mmk20a@my.fsu.edu>
2022-11-17 19:14:52 +0000

    THIS IS NOAH SOLVED ADDED ERROR PAGE FOR ADMIN

--- myproject.py
+++ myproject.py
@@ -379,7 +379,7 @@ def UserProfiles():
     if EMAIL in Admins:
         return render_template("UserProfiles.html",email = Email,items = items,Ditems = Ditems, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
     else:
-        return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+        return render_template("ErrorAdmin.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 #the index function contains the way to call the html file that will be using the data being heald in our database ex
 #APP ROUTE FUNCTION FOR DATABASE CODE EXSAMPLE
 @app.route('/database')
