60cb1ef8ed590ea68934077b3d6d34c109b6bafd
Marlee-Krause <mmk20a@my.fsu.edu>
2022-11-20 19:50:02 +0000

    Upload New File

---
+++ templates/news.js
@@ -0,0 +1,13 @@
+const Like = document.getElementById('Like');
+const Dislike = document.getElementById('Dislike');
+
+Like.addEventListener('click', function onClick() {
+  Like.style.backgroundColor = 'salmon';
+  Like.style.color = 'white';
+});
+
+
+Dislike.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'red';
+  btn.style.color = 'white';
+});
