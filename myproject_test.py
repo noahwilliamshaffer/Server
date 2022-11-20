84defbf68297d8de20024b5348c91b5331bb0a6d
Marlee-Krause <mmk20a@my.fsu.edu>
2022-11-20 17:08:39 +0000

    Update myproject_test.py working on getting user authentification

--- myproject_test.py
+++ myproject_test.py
@@ -1,6 +1,13 @@
 from myproject import app  # Flask instance of the API
-from flaskr.db import get_db
 
+@pytest.fixture()
+def app():
+    app = app()
+    app.config.update({
+        "TESTING": True,
+        "SECRET_KEY": os.getenv('AUTH0_CLIENT_SECRET')
+    })
+    yield app
 
 def test_index_route():
     response = app.test_client().get("/")
@@ -21,7 +28,7 @@ def test_loginout_route():
     assert response.status_code == 302
 
 
-def test_news_route():
+def test_news_route(app):
     response = app.test_client().get("/news")
 
     assert response.status_code == 200
@@ -39,28 +46,27 @@ def test_profile_route():
     assert response.status_code == 200
 
 
-def test_Database():
-    ID = 1
-    con = sqlite3.connect("dislikedArticles.db")
-    cursor = con.execute("DELETE FROM items WHERE id IN (1)")
-    items = cursor.fetchall()
-    cursor.close()
+#def test_Database():
+   # ID = 1
+   # con = sqlite3.connect("dislikedArticles.db")
+   # cursor = con.execute("DELETE FROM items WHERE id IN (1)")
+   # items = cursor.fetchall()
+   # cursor.close()
 
-    Email = 1
-    con = sqlite3.connect("likedArticles.db")
-    cursor = con.execute("SELECT id, email, title, url FROM items")
-    items = cursor.fetchall()
-    cursor.close()
+    #Email = 1
+    #con = sqlite3.connect("likedArticles.db")
+    #cursor = con.execute("SELECT id, email, title, url FROM items")
+    #items = cursor.fetchall()
+    #cursor.close()
 
-    assert response.status_code
+    ##assert response.status_code
 
 
-def test_get_close_db(app):
-    with app.app_context():
-        db = get_db()
-        assert db is get_db()
+#def test_get_close_db(app):
+  #  with app.app_context():
+   ##    assert db is get_db()
 
-    with pytest.raises(sqlite3.ProgrammingError) as e:
-        db.execute("SELECT 1")
+    #with pytest.raises(sqlite3.ProgrammingError) as e:
+     #   db.execute("SELECT 1")
 
-    assert "closed" in str(e.value)
+    #assert "closed" in str(e.value)
